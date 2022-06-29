2086a556aea5dbd6359d1a105da823d51d1b418c
vaibhavlanghe <vaibhavlanghe25@gmail.com>
2022-06-29 20:55:29 +0530

    new swiftUI training started

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Vaibhav Langhe on 29/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Vaibhav Langhe on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
